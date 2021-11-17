8226f31ab3377e5f82d3edb2c4d3f09bd26648a6
Anthony Huang <anthill499@gmail.com>
2021-11-17 17:43:53 -0500

    bcrypto

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+const path = require("path");
+
+const port = 5000;
+
+app.use(express.json());
+app.use(cors());
+
+// test route
+app.get("/test", (req, res) => {
+  res.json({ message: "It's the test route" });
+});
+
+app.listen(port, () => {
+  console.log(`Bcryto server is running on Port ${port}`);
+});
