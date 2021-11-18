ed04b525512309aaf2b01884a38e646c78449e12
JFlec <josephflec@gmail.com>
2021-11-18 00:46:08 -0600

    splash start

--- bcrypto/src/components/Landing/splash.js
+++ bcrypto/src/components/Landing/splash.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './splash.css';
 
 const Splash = () => {
-  return <div id="splash-container"></div>;
+  return (
+    <div id="splash-container">
+      <div id="splash-text"></div>
+      <img src="./undraw_instat_analysis_ajld.svg" alt="splash-art"></img>
+    </div>
+  );
 };
 
 export default Splash;
