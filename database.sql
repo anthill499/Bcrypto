29395118cb8165c9a8844aa247a9caf4a5b0f989
Anthony Huang <anthill499@gmail.com>
2021-11-20 16:11:01 -0500

    config

--- database.sql
+++ database.sql
@@ -1,1 +1,22 @@
-DROP TABLE IF EXISTS users;
+-- RUN THESE COMMAND LINES IN THIS ORDER IF ON WINDOWS WITH SEMICOLON - Anthony
+sudo service postgresql start     
+sudo su - postgres
+psql
+
+\conninfo -- Checks your connection information with PostgreSQL
+
+CREATE DATABASE bcrypto; -- Don't run if you already have one locally
+DROP TABLE IF EXISTS users; -- Don't run if you already have a users table;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; -- Allows us to use uuid in postgresql
+-- Creates User Table;
+CREATE TABLE users( 
+    user_id uuid DEFAULT 
+    uuid_generate_v4(),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT Now(),
+    PRIMARY KEY(user_id) 
+);
+
+
---
+++ postgres.js
@@ -0,0 +1,9 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+// Create a config for every collaborater (diff for each local machine)
+// Anthony's config
+
+// Joe's config
+
+// Alex's config
