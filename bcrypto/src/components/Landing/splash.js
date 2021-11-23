96b6c2bcd80ee9b2328940bdf50ef739361ca577
Anthony Huang <anthill499@gmail.com>
2021-11-23 00:58:13 -0500

    description

--- bcrypto/src/components/Landing/splash.js
+++ bcrypto/src/components/Landing/splash.js
@@ -13,14 +13,16 @@ const Splash = () => {
   return (
     <div id="splash-container">
       <div id="clip" style={{ backgroundColor: misc.artColor }}></div>
-      <div className="splash-text">
+      <div className="splash-text" style={{ color: general.backgroundColor }}>
         <div>
           <h1>Cryptocurrency.</h1>
           <h2>Made simple.</h2>
         </div>
         <p>
-          Stay up to date on the latest cryptocurrency news with Bcrypto, an
-          easy-to-use and intuitive platform made for everyone.
+          Your next crypto wallet. Stay up to date on the latest cryptocurrency
+          news with an easy-to-use and intuitive platform made for everyone.
+          Sign up today and earn up to 7.5% APY. No lock-up or deposit limits
+          guaranteed.
         </p>
         <button
           className="splash-button"
