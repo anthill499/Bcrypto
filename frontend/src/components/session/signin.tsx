166e5dfa45bd5b49668b7614c8b61254c8144237
Anthony Huang <anthill499@gmail.com>
2021-12-04 15:40:18 -0500

    form styled

--- frontend/src/components/session/signin.tsx
+++ frontend/src/components/session/signin.tsx
@@ -8,33 +8,29 @@ const Signin = () => {
   const navigate = useNavigate();
 
   return (
-    <div className={styles.authContainer}>
-      <h3>Bcrypto</h3>
-      <p>Sign in</p>
-      <form>
-        <label htmlFor="username">Username</label>
-        <input
-          name="username"
-          value={username}
-          onChange={(e) => setUsername(e.currentTarget.value)}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.currentTarget.value)}
-        />
-        <button>Sign In</button>
-      </form>
-      <p>
-        New to Bcrypto? Click{" "}
-        <span
-          className={styles.redirectLink}
-          onClick={() => navigate("/register", { replace: true })}
-        >
-          Here
-        </span>
-      </p>
+    <div className={styles.authDiv}>
+      <div className={styles.authContainer}>
+        <h3>Bcrypto</h3>
+        <span className={styles.subLabel}>Sign in</span>
+        <form>
+          <input
+            name="Username"
+            value={username}
+            onChange={(e) => setUsername(e.currentTarget.value)}
+            placeholder="Username"
+          />
+          <input
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.currentTarget.value)}
+            placeholder="Password"
+          />
+          <button>Sign In</button>
+        </form>
+        <p onClick={() => navigate("/register", { replace: true })}>
+          New to Bcrypto?
+        </p>
+      </div>
     </div>
   );
 };
--- frontend/src/components/session/signup.tsx
+++ frontend/src/components/session/signup.tsx
@@ -4,51 +4,65 @@ import styles from "../../styles/auth.module.scss";
 
 const Signup = () => {
   const [username, setUsername] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordTwo, setPasswordTwo] = useState("");
   const navigate = useNavigate();
 
   return (
-    <div className={styles.authContainer}>
-      <h3>Create your Bcrypto account!</h3>
-      <p></p>
-      <form>
-        <label htmlFor="username">Username</label>
-        <input
-          name="username"
-          value={username}
-          onChange={(e) => setUsername(e.currentTarget.value)}
-        />
-        <label htmlFor="password">Email</label>
-        <input
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.currentTarget.value)}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.currentTarget.value)}
-        />
-        <label htmlFor="password2">Confirm Password</label>
-        <input
-          name="password2"
-          value={passwordTwo}
-          onChange={(e) => setPasswordTwo(e.currentTarget.value)}
-        />
-        <button>Create Account</button>
-      </form>
-      <p>
-        Already have an account? Click{" "}
-        <span
-          className={styles.redirectLink}
-          onClick={() => navigate("/login", { replace: true })}
-        >
-          Here
-        </span>
-      </p>
+    <div className={styles.authDiv}>
+      <div className={styles.authContainer}>
+        <h3>Bcrypto</h3>
+        <span className={styles.subLabel}>Create Account</span>
+        <form>
+          <div className={styles.rowContainer}>
+            <input
+              name="firstname"
+              value={firstName}
+              onChange={(e) => setFirstName(e.currentTarget.value)}
+              placeholder="First Name"
+            />
+            <input
+              name="lastname"
+              value={lastName}
+              onChange={(e) => setLastName(e.currentTarget.value)}
+              placeholder="Last Name"
+            />
+          </div>
+          <input
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.currentTarget.value)}
+            placeholder="Username"
+          />
+          <input
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.currentTarget.value)}
+            placeholder="Email"
+          />
+          <div className={styles.rowContainer}>
+            <input
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.currentTarget.value)}
+              placeholder="Password"
+            />
+            <input
+              name="password2"
+              value={passwordTwo}
+              onChange={(e) => setPasswordTwo(e.currentTarget.value)}
+              placeholder="Confirm Password"
+            />
+          </div>
+          <button>Create Account</button>
+        </form>
+        <p onClick={() => navigate("/login", { replace: true })}>
+          Already have an account?
+        </p>
+      </div>
     </div>
   );
 };
