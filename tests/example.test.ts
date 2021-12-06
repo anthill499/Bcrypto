1f69f0c37471fefb3bfea552e59eb287de928bef
Anthony Huang <anthill499@gmail.com>
2021-12-06 17:56:57 -0500

    jest practice

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,5 +1,34 @@
+// Practice
+
 import returnOne from "./example";
+import "regenerator-runtime/runtime";
 
 test("Should return one", () => {
   expect(returnOne()).toBe(1);
+});
+
+async function asyncSample() {
+  return 1;
+}
+
+test("Returns 1 asynchronously", async () => {
+  return asyncSample().then((data) => {
+    expect(data).toBe(1);
+  });
+});
+
+test("Example pass case", () => {
+  expect(1 + 1).toBe(2);
+});
+
+const thisNull = null;
+const thisUndefined = undefined;
+
+function returnOutput(param: any) {
+  return param;
+}
+
+test("Return proper output", () => {
+  expect(returnOutput(thisNull)).toBeNull();
+  expect(returnOutput(thisUndefined)).toBeUndefined();
 });
