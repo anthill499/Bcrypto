d7d7087b619416ec4cfbc3927cd62f206a966276
Anthony Huang <anthill499@gmail.com>
2021-12-18 03:32:14 -0500

    blacked

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./components/landing/landing";
 import Signup from "./components/session/signup";
@@ -8,6 +8,20 @@ import Loading from "./components/loading/loading";
 import { AuthProvider } from "./states/contexts/authContext";
 import { AuthRoute } from "./protectedRoutes";
 const App: React.FC = (): JSX.Element => {
+  useEffect(() => {
+    window.addEventListener("keydown", (event) => {
+      if (event.code === "Space") {
+        window.open("https://leetcode.com/");
+      }
+    });
+    return () =>
+      window.removeEventListener("keydown", (event) => {
+        if (event.code === "Space") {
+          window.open("https://leetcode.com/");
+        }
+      });
+  }, []);
+
   return (
     <AuthProvider>
       <BrowserRouter>
