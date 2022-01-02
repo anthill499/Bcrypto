3902a1ecaf4ff55e58cc2ee924d96dc0160f76df
Anthony Huang <anthill499@gmail.com>
2022-01-02 02:52:59 -0500

    started python env thanks richard

--- frontend/src/components/Chatbox/chatbox.tsx
+++ frontend/src/components/Chatbox/chatbox.tsx
@@ -2,22 +2,25 @@ import styles from "../../styles/chatbox.module.scss";
 import { useState } from "react";
 import ChatBoxModal from "./chatboxModal";
 const ChatboxIcon: React.FC = (): JSX.Element => {
-  const [hasNotifs, setHasNotifs] = useState(false);
+  const [hasNotifs, setHasNotifs] = useState(true);
   const [isModalOpen, setModalOpen] = useState(false);
   return (
+    <div className={styles.chatBoxContainer}>
+      {isModalOpen && <ChatBoxModal />}
+      {hasNotifs && <div className={styles.notificationContainer}>1</div>}
       <div
-        className={styles.chatBoxContainer}
-        onClick={() => setModalOpen(!isModalOpen)}
+        className={styles.chatBoxIcon}
+        onClick={() => {
+          setModalOpen(!isModalOpen);
+          setHasNotifs(!hasNotifs);
+        }}
       >
-        {isModalOpen && <ChatBoxModal />}
-        {hasNotifs && <div className={styles.notificationContainer}>1</div>}
-        <div className={styles.chatBoxIcon}>
-          <img
-            src="https://img.icons8.com/small/50/000000/speech-bubble-with-dots.png"
-            alt="messaging-icon"
-          />
-        </div>
+        <img
+          src="https://img.icons8.com/small/50/000000/speech-bubble-with-dots.png"
+          alt="messaging-icon"
+        />
       </div>
+    </div>
   );
 };
 
--- frontend/src/components/Chatbox/chatboxModal.tsx
+++ frontend/src/components/Chatbox/chatboxModal.tsx
@@ -6,8 +6,9 @@ const ChatBoxModal: React.FC = (): JSX.Element => {
     <div className={styles.chatBoxModal}>
       <div className={styles.chatModalNav}>
         <CloseIcon style={{ fontSize: "large" }} />
-        <MinimizeIcon style={{ fontSize: "large" }}/>
+        <MinimizeIcon style={{ fontSize: "large" }} />
       </div>
+      <div className={styles.actualChatBox}></div>
     </div>
   );
 };
